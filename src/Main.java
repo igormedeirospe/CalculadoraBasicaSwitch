1f192f86bfab6c72b65ff5e4f79d4438db837a01
Igor <igormedeiros407@gmail.com>
2024-07-17 19:40:33 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //calculadora basica = Adição - Multiplicação - Subtração - divisão
+        //texto explicando como utilizar a calculadora
+        System.out.println("Digite 1 para Adição");
+        System.out.println("Digite 2 para Subtração");
+        System.out.println("Digite 3 para Multiplicação");
+        System.out.println("Digite 4 para Divisão");
+        Scanner objetoConta = new Scanner(System.in);
+        System.out.println("Digite o numero que voce irá usar");
+        int NumeroAcao = objetoConta.nextInt();
+
+        switch (NumeroAcao){
+            case 1: //Adição
+                int numero, numero2;
+                numero = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero da soma"));
+                numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero da soma"));
+                int soma;
+                soma = (numero + numero2);
+                System.out.println("O valor da soma é: " + soma);
+        break;
+            case 2://Subtração
+            int numeroSub, numeroSub2;
+            numeroSub = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero da sub"));
+            numeroSub2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero da sub"));
+            int sub;
+            sub = (numeroSub - numeroSub2);
+            System.out.println("O valor da Subtração é: " + sub);
+                break;
+
+            case 3://Multiplicação
+                int numeroMult1, numeroMult2;
+                numeroMult1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero da Multiplicação"));
+                numeroMult2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero da Multiplicação"));
+                int Multi;
+                Multi = (numeroMult1 * numeroMult2);
+                System.out.println("O valor da Multiplicação é: " + Multi);
+                break;
+
+            case 4://divisão
+                int numeroDiv, numeroDiv2;
+                numeroDiv = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero da divisão"));
+                numeroDiv2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero da divisão"));
+                int div;
+                div = (numeroDiv / numeroDiv2);
+                System.out.println("O valor da Divisão é: " + div);
+                break;
+            default:
+                System.out.println("nada");
         }
     }
 }
